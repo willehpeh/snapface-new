3c173c15e0b32d681d27edfbdbc4eddb863027b6
w.alexander <will_alexander@me.com>
2024-03-05 16:29:45 +0100

    End of P2C1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    FaceSnapComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'snapface';
-}
+export class AppComponent {}
---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent {
+
+}
