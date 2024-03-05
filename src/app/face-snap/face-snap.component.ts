71765aa4148f3f0f2793acffab376e7b5ff4ec73
w.alexander <will_alexander@me.com>
2024-03-05 16:57:36 +0100

    End of P2C2A

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {}
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+
+  ngOnInit(): void {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis toujours !';
+    this.createdAt = new Date();
+    this.snaps = 0;
+  }
+}
