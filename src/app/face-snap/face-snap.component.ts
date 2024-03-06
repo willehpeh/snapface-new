1af0bfa735efec7aef5c57e70ef9ed13b4cf0a14
w.alexander <will_alexander@me.com>
2024-03-06 16:06:39 +0100

    End of P2C3

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -13,6 +13,8 @@ export class FaceSnapComponent implements OnInit {
   createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
 
   ngOnInit(): void {
     this.title = 'Archibald';
@@ -20,9 +22,27 @@ export class FaceSnapComponent implements OnInit {
     this.createdAt = new Date();
     this.snaps = 5;
     this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
   }
 
-  onAddSnap(): void {
+  onSnap(): void {
+    if (this.userHasSnapped) {
+      this.unSnap();
+    } else {
+      this.snap();
+    }
+  }
+
+  unSnap() {
+    this.snaps--;
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
+  }
+
+  snap() {
     this.snaps++;
+    this.snapButtonText = 'Oops, unSnap!';
+    this.userHasSnapped = true;
   }
 }
