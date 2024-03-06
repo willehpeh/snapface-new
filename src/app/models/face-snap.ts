038ae3a28ebc9a0b42190adf94dfde0e759aa7b3
w.alexander <will_alexander@me.com>
2024-03-06 16:30:25 +0100

    End of P2C4A

---
+++ src/app/models/face-snap.ts
@@ -0,0 +1,7 @@
+export class FaceSnap {
+  constructor(public title: string,
+              public description: string,
+              public createdAt: Date,
+              public snaps: number,
+              public imageUrl: string) {}
+}
