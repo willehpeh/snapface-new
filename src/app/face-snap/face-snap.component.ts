b9ccf51e750d12295f6f6faf48a01ff27779e8cb
w.alexander <will_alexander@me.com>
2024-03-19 15:24:28 +0100

    End of P4C2A

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
-import { LowerCasePipe, NgClass, NgStyle, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
@@ -9,8 +9,7 @@ import { LowerCasePipe, NgClass, NgStyle, TitleCasePipe, UpperCasePipe } from '@
     NgStyle,
     NgClass,
     UpperCasePipe,
-    LowerCasePipe,
-    TitleCasePipe
+    DatePipe
   ],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
