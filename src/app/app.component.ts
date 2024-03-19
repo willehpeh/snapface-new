b1b4ed7ca3b8cad4c2e599b16fd035d07735779b
w.alexander <will_alexander@me.com>
2024-03-19 10:54:26 +0100

    End of P3C2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,33 +12,38 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+
+  faceSnaps!: FaceSnap[];
+
   mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
   myLastSnap!: FaceSnap;
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis toujours !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      10
-    );
-    this.myOtherSnap = new FaceSnap(
-      'Three Rock Mountain',
-      'Un endroit magnifique pour les randonnées.',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-      new Date(),
-      6
-    );
-    this.myLastSnap = new FaceSnap(
-      'Un bon repas',
-      'Mmmh que c\'est bon !',
-      'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-      new Date(),
-      156
-    );
+    this.faceSnaps = [
+      new FaceSnap(
+        'Archibald',
+        'Mon meilleur ami depuis toujours !',
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        new Date(),
+        10
+      ),
+      new FaceSnap(
+        'Three Rock Mountain',
+        'Un endroit magnifique pour les randonnées.',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        new Date(),
+        6
+      ),
+      new FaceSnap(
+        'Un bon repas',
+        'Mmmh que c\'est bon !',
+        'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        new Date(),
+        156
+      )
+    ];
 
-    this.myOtherSnap.setLocation('à la montagne');
+    this.faceSnaps[1].setLocation('à la montagne');
   }
 }
