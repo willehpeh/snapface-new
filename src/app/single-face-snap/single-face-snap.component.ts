8f55d01ad1071b0061279c7622b00dfd313c930e
w.alexander <will_alexander@me.com>
2024-03-27 14:54:01 +0100

    End of P5C5A

---
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
+import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
+import { FaceSnapsService } from '../services/face-snaps.service';
+
+@Component({
+  selector: 'app-single-face-snap',
+  standalone: true,
+  imports: [
+    NgStyle,
+    NgClass,
+    UpperCasePipe,
+    DatePipe,
+  ],
+  templateUrl: './single-face-snap.component.html',
+  styleUrl: './single-face-snap.component.scss'
+})
+export class SingleFaceSnapComponent implements OnInit {
+  @Input() faceSnap!: FaceSnap;
+
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
+
+  constructor(private faceSnapsService: FaceSnapsService) {}
+
+  ngOnInit(): void {
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
+  }
+
+  onSnap(): void {
+    if (this.userHasSnapped) {
+      this.unSnap();
+    } else {
+      this.snap();
+    }
+  }
+
+  unSnap() {
+    this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
+  }
+
+  snap() {
+    this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
+    this.snapButtonText = 'Oops, unSnap!';
+    this.userHasSnapped = true;
+  }
+}
